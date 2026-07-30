ff79f87ef42b93ebfbd75be11c9fba399bb3a645
Jairo <jairosouza67@gmail.com>
2026-07-29 22:02:27 -0300

    feat: add updateUserCustomerId cloud function to manage Asaas customer IDs with CORS and authentication validation

--- functions/src/api/updateUserCustomerId.js
+++ functions/src/api/updateUserCustomerId.js
@@ -28,7 +28,7 @@ exports.updateUserCustomerId = functions.https.onRequest(async (req, res) => {
   if (req.method === 'OPTIONS') {
     res.set('Access-Control-Allow-Origin', resolveOrigin(req.headers.origin || ''));
     res.set('Access-Control-Allow-Methods', 'POST');
-    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.status(204).send('');
     return;
   }
